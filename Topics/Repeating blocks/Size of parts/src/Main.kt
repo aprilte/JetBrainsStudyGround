657eac573ce870590cd15845a8b53cfbaf5b934c
aprilte <apriltexs@gmail.com>
2022-09-22 20:43:55 +0900

    0922::2

--- Topics/Repeating blocks/Size of parts/src/Main.kt
+++ Topics/Repeating blocks/Size of parts/src/Main.kt
@@ -1,3 +1,25 @@
 fun main() {
     // put your code here
+    val repeater: Int = readln().toInt()
+    var retArray = Array(3) { 0 * it }
+    repeat(repeater) {
+        val readVal: Int = readln().toInt()
+        when (readVal) {
+            0 -> retArray[0] = retArray[0]?.plus(1)
+            1 -> retArray[1] = retArray[1]?.plus(1)
+            -1 -> retArray[2] = retArray[2]?.plus(1)
+        }
+    }
+    retArray.forEach {
+        print("$it ")
+    }
 }
+//fun main() {
+//    var i = 0
+//    while (i < 10) {
+//        i++
+//        if (i % 2 == 0) {
+//            print("$i ")
+//        }
+//    }
+//}
