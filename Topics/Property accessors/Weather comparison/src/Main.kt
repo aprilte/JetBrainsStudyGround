3722f86e0146225737005876ca3144312b2319ed
aprilte <apriltexs@gmail.com>
2022-09-27 01:30:51 +0900

    0927::1

--- Topics/Property accessors/Weather comparison/src/Main.kt
+++ Topics/Property accessors/Weather comparison/src/Main.kt
@@ -1,5 +1,19 @@
 class City(val name: String) {
-    var degrees: Int
+    var degrees: Int = 0
+        get() {
+            return field
+        }
+        set(value) {
+            if (value < -92 || 57 < value) {
+                when (name) {
+                    "Moscow", -> field = 5
+                    "Hanoi", -> field = 20
+                    "Dubai", -> field = 30
+                }
+            } else {
+                field = value
+            }
+        }
 }        
 
 fun main() {
@@ -13,6 +27,19 @@ fun main() {
     val thirdCity = City("Hanoi")
     thirdCity.degrees = third
 
-    //implement comparing here
-    print()
+    // implement comparing here
+    val min = minOf(firstCity.degrees, secondCity.degrees, thirdCity.degrees)
+    val list = listOf(firstCity.degrees, secondCity.degrees, thirdCity.degrees)
+    val listbyset = list.toSet().toList()
+    if ( list.size != listbyset.size) {
+        print("neither")
+    } else {
+        if (min == firstCity.degrees) {
+            println(firstCity.name)
+        } else if (min == secondCity.degrees) {
+            println(secondCity.name)
+        } else {
+            println(thirdCity.name)
+        }
+    }
 }
