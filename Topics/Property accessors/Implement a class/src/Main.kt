1f1981ac792788d0a7091b394f9b5b57f319bded
aprilte <apriltexs@gmail.com>
2022-09-26 20:44:28 +0900

    0926::2

---
+++ Topics/Property accessors/Implement a class/src/Main.kt
@@ -0,0 +1,28 @@
+// write your class here
+class LewisCarrollBook {
+    var name: String = ""
+        get() {
+            println("The name of the book is $field")
+            return field
+        }
+        set(value) {
+            field = value
+            println("Now, a book called $field")
+        }
+    var author: String = "Lewis Carroll"
+        get() {
+            println("The author of the book is $field")
+            return field
+        }
+
+    var price: Int = 0
+        get() {
+            println("Putting a new price...")
+            return field
+        }
+        set(value) {
+            field = value
+            println("The new price is $field")
+        }
+
+}
